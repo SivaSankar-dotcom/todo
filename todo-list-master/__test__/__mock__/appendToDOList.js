8c7ff5f874406c6d7609c1bc5a44bc94027277f4
Siva Sankar <rameshsankarsiva@gmail.com>
2024-08-27 10:51:28 +0530

    Add files via upload

---
+++ todo-list-master/__test__/__mock__/appendToDOList.js
@@ -0,0 +1,17 @@
+const appendTodoList = (todo) => {
+  document.body.innerHTML = `  
+  <div class = 'row-elements'>
+  <div class="todo-left">
+    <input type="checkbox" class="status" id = ${todo.index}  ${todo.completed ? 'checked' : ''} />
+    <p class="description ${todo.completed ? 'strike-through' : ''}">${todo.description}</p>
+  </div>
+  <button class="btn btn-edit">
+    <i class="bi bi-three-dots-vertical"></i>
+  </button>  
+  <button class="btn btn-delete">  
+    <i class="bi bi-trash"></i>
+  </button>
+  </div>`;
+};
+
+module.exports = appendTodoList;
